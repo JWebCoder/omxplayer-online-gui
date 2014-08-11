abe513bd00658ca513f5d695a0e41a8a59729da6
João Moura <joao.p.moura@parceiros.zonoptimus.pt>
2014-08-11 15:16:57 +0100

    needed stuff

---
+++ usefull.py
@@ -0,0 +1,36 @@
+command_list={
+'speedup':'1',
+'speeddown':'2',
+'nextaudio':'k',
+'prevaudio':'j',
+'nextchapter':'o',
+'prevchapter':'i',
+'nextsubs':'m',
+'prevsubs':'n',
+'togglesubs':'s',
+'stop':'q',
+'pause':'p',
+'volumedown':'-',
+'volumeup':'+',
+'languagedown':'j',
+'languageup':'k',
+'seek-30':'\x1b\x5b\x44',
+'seek+30':'\x1b\x5b\x43',
+'seek-600':'\x1b\x5b\x42',
+'seek+600':'\x1b\x5b\x41'}
+
+subprocess.Popen('killall omxplayer.bin',stdout=subprocess.PIPE,shell=True)
+subprocess.Popen('clear',stdout=subprocess.PIPE,shell=True)
+subprocess.Popen('omxplayer -o hdmi ' + file + ' <' + OMXIN_FILE ,shell=True)
+omx_send('.')
+
+def omx_send(command):
+    subprocess.Popen('echo -n '+data+' >'+re.escape(OMXIN_FILE),shell=True)
+    return 1
+    
+def send_text_command(command):
+    if command in command_list:
+        omx_send(command_list[name])
+        return 1
+    else:
+        return 0
