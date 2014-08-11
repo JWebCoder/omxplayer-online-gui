e9eee46f82bce924b839c7119b41564e840fcf24
João Moura <joao87moura@gmail.com>
2014-08-11 23:06:24 +0100

    Folder name syntax

--- omxplayer/views.py
+++ omxplayer/views.py
@@ -15,7 +15,7 @@ def index(request):
 def list_files(request):
     files = []
     directories = []
-    path = "/Media/"
+    path = "/media/"
     prevFolder = ""
     if 'dir' in request.GET:
         path = request.GET["dir"]
