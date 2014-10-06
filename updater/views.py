2fe035b33c682e33268d505971b4d4d23daf85b6
João Moura <joao87moura@gmail.com>
2014-10-06 20:43:42 +0100

    path fix

--- updater/views.py
+++ updater/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import git 
-
+import os
 
 # Create your views here.
 def index(request):
-    g = git.cmd.Git('~/raspberry/')
+    
+    BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+    g = git.cmd.Git(BASE_DIR)
     g.pull()
     return render(request, 'updater/home.html')
