74586453ee84ea991b26ab6f2201897107b3e3be
João Moura <joao.p.moura@parceiros.zonoptimus.pt>
2014-10-06 18:10:31 +0100

    fix js and css loader. added more options to manga reader. fix in chapter html. views data. fixes to css

--- mangareader/views.py
+++ mangareader/views.py
@@ -15,9 +15,10 @@ def list_mangas(request):
 
 def open_chapter(request):
     name = request.GET['name']
-    pages = MangaPanda.series(name).chapters[int(request.GET['pos'])].pages
+    pos = int(request.GET['pos'])
+    pages = MangaPanda.series(name).chapters[pos].pages
     length = len(pages)
-    return render(request, 'manga/chapter.html', { 'name': name, 'pages': pages, 'len': length})
+    return render(request, 'manga/chapter.html', { 'name': name, 'pages': pages, 'num': pos})
 
 
 def read_manga(request):
